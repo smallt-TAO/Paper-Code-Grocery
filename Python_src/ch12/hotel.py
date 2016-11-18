272202dd8bd12a1c1b7e4d014e01286e05bb56f3
smallt-TAO <llt5662720@163.com>
2016-11-18 23:44:31 +0800

    Update
    Add update.

---
+++ Python_src/ch12/hotel.py
@@ -0,0 +1,17 @@
+__author__ = 'Smalltao'
+
+
+class HotelRoomCalc(object):
+    """Hotel room rate calculator"""
+
+    def __init__(self, rt, sales=0.085, rm=0.1):
+        """Hotel RoolCalc default arguments:
+        sales tax == 8.5% and room tax == 10%"""
+        self.salesTax = sales
+        self.roomTax = rm
+        self.roomRate = rt
+
+    def calc_total(self, days=1):
+        """Calculate total; default to daily rate"""
+        daily = round((self.roomRate * (1 + self.roomTax + self.salesTax)), 2)
+        return float(days) * daily
