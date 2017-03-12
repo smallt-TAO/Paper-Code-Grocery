e4a1c2f72545e4007d57a699ffa12791be903e77
smallt-TAO <llt5662720@163.com>
2017-03-12 20:52:53 +0800

    #20170312 Try to commit.

---
+++ Structure data/list/list/list/list.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "time.h"
+#include "io.h"
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 20
+
