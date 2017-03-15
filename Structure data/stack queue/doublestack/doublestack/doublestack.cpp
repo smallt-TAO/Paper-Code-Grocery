16903885ca3c63b8262b80d53e79ccc83cb0c22c
smallt-TAO <llt5662720@163.com>
2017-03-15 13:18:09 +0800

    #20170315 Small trouble.

---
+++ Structure data/stack queue/doublestack/doublestack/doublestack.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "time.h"
+#include "math.h"
+#include "io.h"
+#include "stdlib.h"
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 20
